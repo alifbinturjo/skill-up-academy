53514807ad0fe0009bb64f4b1e05d9315af7f4c9
alifbinturjo <alif.turjo@northsouth.edu>
2025-07-27 15:21:52 +0600

    log

--- auth/logout.php
+++ auth/logout.php
@@ -1,22 +1,8 @@
 <?php
 session_start();
 include'cnct.php';
-
 session_unset();
 session_destroy();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Logging out</title>
-</head>
-<body>
-    
-</body>
-</html>
-<?php
 $conn->close();
 header("Location: ../index.php");
 exit();
